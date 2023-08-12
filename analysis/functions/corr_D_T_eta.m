abf56e6236ba8ef366d5c61fe11175008a44b386
Sören J. Gerke <421016+git@mailbox.org>
2023-08-13 00:36:41 +0200

    fixup! fixes diffusivity temperature correction

---
+++ analysis/functions/corr_D_T_eta.m
@@ -0,0 +1,11 @@
+##  SPDX-License-Identifier: BSD-3-Clause
+##  Copyright (c) 2023, Sören Jakob Gerke
+
+## diffusivity correction for small temperature delta based on the
+## Stokes-Einstein proportionality
+##
+## Author: Sören J. Gerke
+
+function D_c = corr_D_T_eta (eta_ref, T_ref, D_ref, eta_c, T_c)
+  D_c = eta_ref ./ T_ref .* D_ref .* 1 ./ (eta_c ./ T_c);
+endfunction
